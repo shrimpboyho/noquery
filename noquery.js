76a6aae062b00086e9ae3dd9073931654e1da43a
shrimpboyho <shrimpboyhoftw@gmail.com>
2013-08-15 15:27:20 -0400

    Stuff

--- noquery.js
+++ noquery.js
@@ -2,5 +2,31 @@
 
 (function() {
 
-  
+  function n$( selector, context ) {
+    context = context || document;
+
+    return {
+
+      all: function() {
+        var list, ary = [];
+        list = context.querySelectorAll( selector );
+        for ( var i = 0; i < list.length; i++ ) {
+          ary[ i ] = list[ i ];
+        }
+        return ary;
+      },
+
+      first: function() {
+        return context.querySelector( selector );
+      },
+
+      last: function() {
+        var list = context.querySelectorAll( selector );
+        return list.length > 0 ? list[ list.length - 1 ] : null;
+      }
+
+    };
+
+  }
+
 }());
